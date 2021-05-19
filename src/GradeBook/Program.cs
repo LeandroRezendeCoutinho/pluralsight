46981c6148f6db0af5f849482db1c29d88c9e002
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2021-05-19 08:12:03 -0300

    declare a double array

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GradeBook
 {
@@ -6,7 +7,14 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var numbers = new[] { 12.7, 10.3, 6.11, 4.1 };
+
+            var result = 0.0;
+            foreach (var number in numbers)
+            {
+                result += number;
+            }
+            Console.WriteLine($"The average grade is {result}");
         }
     }
 }
