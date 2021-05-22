850935964a0709a8a5f42914ed476f85a9ebc7e4
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2021-05-22 10:47:07 -0300

    switch with patterns matching

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -21,15 +21,15 @@ namespace GradeBook
                 case 'A':
                     AddGrade(90);
                     break;
-                
+
                 case 'B':
                     AddGrade(80);
                     break;
-                
+
                 case 'C':
                     AddGrade(70);
                     break;
-                
+
                 default:
                     AddGrade(0);
                     break;
@@ -45,19 +45,33 @@ namespace GradeBook
 
             for (var index = 0; index < grades.Count; index++)
             {
-                if (grades[index] == 42.1)
-                {
-                    goto done;
-                }
-                
                 result.Low = Math.Min(grades[index], result.Low);
                 result.High = Math.Max(grades[index], result.High);
                 result.Average += grades[index];
-                index += 1;
             };
 
             result.Average /= grades.Count;
-            done:
+
+            switch (result.Average)
+            {
+                case var d when d >= 90.0:
+                    result.Letter = 'A';
+                    break;
+
+                case var d when d >= 80.0:
+                    result.Letter = 'B';
+                    break;
+                case var d when d >= 70.0:
+                    result.Letter = 'C';
+                    break;
+                case var d when d >= 60.0:
+                    result.Letter = 'D';
+                    break;
+                default:
+                    result.Letter = 'F';
+                    break;
+            }
+
             return result;
         }
 
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -16,6 +16,7 @@ namespace GradeBook
             Console.WriteLine($"The average grade is {stats.Average:N1}");
             Console.WriteLine($"The low grade is {stats.Low:N1}");
             Console.WriteLine($"The high grade is {stats.High:N1}");
+            Console.WriteLine($"The letter grade is {stats.Letter}");
         }
     }
 }
