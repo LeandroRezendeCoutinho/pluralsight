58f0e56af16318abba1c7ba2ebbb85799389148e
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2021-05-22 14:50:51 -0300

    use two delegates in one declaration

--- src/test/GradeBook.Tests/TypeTests.cs
+++ src/test/GradeBook.Tests/TypeTests.cs
@@ -7,17 +7,29 @@ namespace GradeBook.Tests
         
     public class TypeTests
     {
+        private int count = 0;
+        
         [Fact]
         public void WriteLogDelegateCanPointToMethod()
         {
-            WriteLogDelegate log;
-            log = ReturnMessage;
+            WriteLogDelegate log = ReturnMessage;
+            log += ReturnMessage;
+            log += IncrementCount;
+            
             var result = log("Hello");
-            Assert.Equal("Hello", result);
+            
+            Assert.Equal(3, count);
         }
 
+        string IncrementCount(string message)
+        {
+            count++;
+            return message.ToLower();
+        }
+        
         string ReturnMessage(string message)
         {
+            count++;
             return message;
         }
         
