c1e7b4e353514120ffbab849f43fb56c347f764e
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2021-05-24 08:21:18 -0300

    delegate use example

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -8,6 +8,7 @@ namespace GradeBook
         static void Main(string[] args)
         {
             var book = new Book("Scott's book");
+            book.GradeAdded += OnGradeAdded;
 
             while (true)
             {
@@ -43,11 +44,16 @@ namespace GradeBook
             var stats = book.GetStatistics();
             
             Console.WriteLine(Book.CATEGORY);
-            Console.WriteLine($"For the book named{book.Name}");
+            Console.WriteLine($"For the book named {book.Name}");
             Console.WriteLine($"The average grade is {stats.Average:N1}");
             Console.WriteLine($"The low grade is {stats.Low:N1}");
             Console.WriteLine($"The high grade is {stats.High:N1}");
             Console.WriteLine($"The letter grade is {stats.Letter}");
+        }
+
+        static void OnGradeAdded(Object sender, EventArgs e)
+        {
+            Console.WriteLine("A grade was added");
         }
     }
 }
